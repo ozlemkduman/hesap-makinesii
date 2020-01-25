71a03410c7c4151ddb4af6ae7f9d92b11513839b
Özlem Karaduman <ozlemk.duman@gmail.com>
2020-01-25 17:28:36 +0300

    algoritma yazıldu

---
+++ script.js
@@ -0,0 +1,37 @@
+//HESAP MAKİNESİ
+
+// Arayüzde 0 1 2 3 4 5 6 7 8 9 C + - % * / = <- tuşları olucak
+//input bölümü boş olacak
+// rakamlara tıklanınca inputta tıklanan rakam gözükcek
+// hangi işlem operatörü çağırılmışsa ona göre işlem yapacak = e basınca sonuç inputa yazdırılacak
+// <- son rakamı ya da seçilen işlemi (+ - * %) siler
+// C tüm işlemleri sıfırlayacak
+
+
+// algoritma
+
+// uygulama açıldığında
+    // rakamlara basabilir
+        //izin ver
+        //girilen rakam inputta gözüksün
+
+    //operatörlere basabilir
+        //rakamdan önce
+            //izin verme
+        //rakamdan sonra
+            //izin ver<15>
+
+    // = tuşuna basabilir
+        // girilen iki rakam arası operatöre göre işlem yapılır.
+        // 2+2= e bastıysa inputa 4 yazdır
+        // 5-3= e bastıysa inputa 2 yazdır.
+        //10/2 = e bastıysa inputa 5 yazdır
+        // 5*2 = e bastıysa inputa 10 yazdır.
+        // 100  %  30 = e basarsa ekranda 30 yazdır.
+        
+    // c Tuşuna basabilir
+        // c tuşuna basıldığında inputı temizler
+        //uygulamayı sıfırlar
+
+    //<- tuşuna basabilir
+        // önce yazılan inputu ya da operatörü siler
