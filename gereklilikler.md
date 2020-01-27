2f28eb8dc853cf6916c783345f1e022fd7a77090
Özlem Karaduman <ozlemk.duman@gmail.com>
2020-01-27 16:13:46 +0300

    tüm proje

---
+++ gereklilikler.md
@@ -0,0 +1,30 @@
+Hesap Makinesi
+
+0 1 2 3 4 5 6 7 8 9 C + - % * = <- tuşları olur
+
+C
+Yapılan tüm işlemleri sıfırlar
+<-
+Son rakamı ya da seçilen işlemi (+ - * %) siler
+Güzel tasarımlı olcak ekteki gibi
+Gereklilikleri liste halinde yaz
+   Belli kullanım senaryoları (Sırasıyla 3 + 8 - 5 = tuşlarına basınca sonucun 6 olması)
+Algoritmayı yaz
+Kod yazma şimdilik
+
+Kullanılacak bilgiler
+Functions
+Events
+Scope (https://tylermcginnis.com/var-let-const/)
+
+Git'e ekle projeyi
+
+git init (klasörü git projesi haline getirir)
+git add . (hepsini ekler)
+git commit -m "mesaj kafana göre"
+github a gir, yeni repository oluştur
+…or push an existing repository from the command line
+başlığının altındaki 2 satırı sırayla çalıştır
+
+repository > settings > collaborators a gir
+hakankaraduman ismini ekleyip beni davet etmiş olcaksın
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //HESAP MAKİNESİ
 
-// Arayüzde 0 1 2 3 4 5 6 7 8 9 C + - % * / = <- tuşları olucak
+// Arayüzde 0 1 2 3 4 5 6 7 8 9 C + -  * / = <- tuşları olucak
 //input bölümü boş olacak
 // rakamlara tıklanınca inputta tıklanan rakam gözükcek
 // hangi işlem operatörü çağırılmışsa ona göre işlem yapacak = e basınca sonuç inputa yazdırılacak
@@ -9,29 +9,84 @@
 
 
 // algoritma
+// rakam{bir; iki; üc; dört; bes; alti; yedi; sekiz; dokuz; sifir;}; esittir() 
+//toplama(); cikarma(); carpma(); bolme(); silme(); sifirla(); inputVal;  
+//fonksiyonlar:
+    // esittir()
+        //eğer tıklanan toplama ise a ve b yi topla
+        //eğer tıklanan cikarma ise a dan b yi cıkar
+        //eğer tıklanan carpma ise a ve b yi çarp
+        //eğer tıklanan bölme ise a yı b ye böl
+    //toplama(a,b){
+        //let topla=a+b;
+        //return topla;
+    //}
+
+    //cikarma(a,b){
+        //let cikar=a-b;
+        //return cikar;
+    //}
+
+    //carpma(a,b){
+        //let carp=a*b;
+        //return carp;
+    //}
+
+    //bolme(a,b){
+        //let bol=a/b;
+        //return bol;
+    //}
 
 // uygulama açıldığında
     // rakamlara basabilir
         //izin ver
-        //girilen rakam inputta gözüksün
+        //girilen rakam inputta gözüksün = inputVal
+            
 
     //operatörlere basabilir
         //rakamdan önce
             //izin verme
         //rakamdan sonra
-            //izin ver<15>
+            //izin ver
 
     // = tuşuna basabilir
-        // girilen iki rakam arası operatöre göre işlem yapılır.
-        // 2+2= e bastıysa inputa 4 yazdır
-        // 5-3= e bastıysa inputa 2 yazdır.
-        //10/2 = e bastıysa inputa 5 yazdır
-        // 5*2 = e bastıysa inputa 10 yazdır.
-        // 100  %  30 = e basarsa ekranda 30 yazdır.
+        //esittir()
+            // girilen iki rakam arası operatöre göre işlem yapılır.
+            // 2+2= e bastıysa inputa 4 yazdır
+            // 5-3= e bastıysa inputa 2 yazdır.
+            //10/2 = e bastıysa inputa 5 yazdır
+            // 5*2 = e bastıysa inputa 10 yazdır.
+            // 100  %  30 = e basarsa ekranda 30 yazdır.
+            
         
     // c Tuşuna basabilir
-        // c tuşuna basıldığında inputı temizler
-        //uygulamayı sıfırlar
+        //sifirla()    
+            //uygulamayı sıfırlar
 
     //<- tuşuna basabilir
-        // önce yazılan inputu ya da operatörü siler
+        //silme()
+            //tıklandığında inputttan son rakamı veya operatörü siler.
+
+
+let rakam={bir, iki, üc, dört, bes, alti, yedi, sekiz, dokuz, sifir};
+function toplama(a,b){
+    let topla=a+b;
+    return topla;
+}
+
+function cikarma(a,b){
+    let cikar=a-b;
+    return cikar;
+}
+
+function carpma(a,b){
+    let carp=a*b;
+    return carp;
+}
+
+function bolme(a,b){
+    let bol=a/b;
+    return bol;
+}
+
+            
